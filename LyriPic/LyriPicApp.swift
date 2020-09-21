3ddd9a832c723e21d3840fb03864f570bdb1904f
Emmanuel <enkkashley@gmail.com>
2020-09-21 16:40:49 +0000

    Initial Commit

---
+++ LyriPic/LyriPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyriPicApp.swift
+//  LyriPic
+//
+//  Created by Emmanuel Ashley on 21/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct LyriPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
